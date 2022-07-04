5453c7ae6b93726926401526919a5af6edd07a97
uinone <tizm423@gmail.com>
2022-07-05 03:17:21 +0900

    [Refector] refectoring codes for rounting to deploy app

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,8 +6,8 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/:coinId/*" element={<Coin />}></Route>
-        <Route path="/" element={<Coins />}></Route>
+        <Route path="/Coin-Tracker/:coinId/*" element={<Coin />}></Route>
+        <Route path="/Coin-Tracker/" element={<Coins />}></Route>
       </Routes>
     </BrowserRouter>
   );
